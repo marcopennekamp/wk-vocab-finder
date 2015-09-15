115b155939b7981e45ce0872aac7d54971666131
Marco Pennekamp <marco.pennekamp96@gmail.com>
2015-09-15 19:50:16 +0200

    Add 2k vocab list. Use loops in the mark_all script.

--- mark_all.sh
+++ mark_all.sh
@@ -6,13 +6,9 @@ mrk() {
 
 rm -rf leveled
 mkdir leveled
-mrk 1-1
-mrk 1-2
-mrk 1-3
-mrk 1-4
-mrk 1-5
-mrk 1-6
-mrk 1-7
-mrk 1-8
-mrk 1-9
-mrk 1-10
+
+for c in `seq 1 2`; do
+    for s in `seq 1 10`; do
+        mrk $c-$s
+    done
+done
